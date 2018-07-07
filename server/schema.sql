48e48bfcfa1e3ebf6c405c31424b41407aaa5654
burkegg <burkegreen@gmail.com>
2018-07-06 17:26:51 -0700

    Created Chatterz database!

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,32 @@
-CREATE DATABASE chat;
+CREATE DATABASE chatterz;
 
-USE chat;
+USE chatterz;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE Users (
+  id int NOT NULL,
+  name varchar(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE Room (
+  id int NOT NULL,
+  roomname varchar(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE Messages (
+  id int NOT NULL,
+  message varchar(255),
+  userID int,
+  roomID int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(userID) REFERENCES Users (id),
+  FOREIGN KEY(roomID) REFERENCES Room (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
 
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
